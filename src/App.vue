f120caa034808a78ac48b6b3cf92404a1926869d
chenhb <chenhb@tritonsfs.com>
2018-04-13 15:31:30 +0800

    all

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <group>
+      <cell title="title" value="value"></cell>
+    </group>
+    <x-switch :title="title" v-model="show1"></x-switch>
+
+    <actionsheet v-model="show1" :menus="menus1" @on-click-menu="click"></actionsheet> 
+  </div>
+</template>
+<script>
+import { Group, Cell,Alert ,Actionsheet,TransferDom,XSwitch,Toast   } from 'vux'
+export default {
+  name: 'App',
+  components: {
+    Group,
+    Cell,
+    XSwitch,
+    Actionsheet ,
+    Alert ,
+    Toast
+  },
+   directives: {
+    TransferDom
+  },
+  data() {
+    return {
+      title:"usage basic",
+      show1: false,
+      menus1: {
+        menu1:"yes",
+        menu2:"no"
+      }
+    }
+  },
+  methods: {
+    
+    click (key) {
+      console.log(key)
+    },
+    
+  }
+};
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import { AlertPlugin, ToastPlugin } from 'vux'
+
+Vue.use(AlertPlugin)
+Vue.use(ToastPlugin)
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+});
